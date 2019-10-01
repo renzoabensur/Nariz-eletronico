f4cddb49200b23653cde208153602283e12d56f9
Renzo <renzo.abensur@thunderatz.org>
2019-10-01 14:15:53 -0300

    Recover

--- Recover.ino
+++ Recover.ino
@@ -12,7 +12,7 @@ static uint32_t ticks = 0;
 static uint32_t time_liberacao_ms = 0;    // Tempo de liberacao recebido pelo terminal
 static uint32_t time_recuperacao_ms = 0;  // Tempo de recuperacao recebido pelo terminal
 static uint32_t ciclos = 0;               // Numero de ciclos recebido pelo terminal
-
+float numPlots = 0;
 int count = 0;                            // Contagem de ciclos
 
 float valor_sensor1 = 0;                   // Valor recebido do sensor
@@ -52,16 +52,16 @@ bool libera = true;                       // Verdadeiro se estiver liberando gas
 bool recupera = false;                    // 
 bool start_command = false;               // Verdadeiro se comecar a leitura do sensor
 
-double valor_ppm1 = 0;                     // Valor da leitura do sensor de gas em PPM
-double valor_ppm2 = 0;
-double valor_ppm3 = 0;
-double valor_ppm4 = 0;
-double valor_ppm5 = 0;
-double valor_ppm6 = 0;
-double valor_ppm7 = 0;
-double valor_ppm8 = 0;
-double valor_ppm9 = 0;
-double valor_ppm10 = 0;
+float valor_ppm1 = 0;                     // Valor da leitura do sensor de gas em PPM
+float valor_ppm2 = 0;
+float valor_ppm3 = 0;
+float valor_ppm4 = 0;
+float valor_ppm5 = 0;
+float valor_ppm6 = 0;
+float valor_ppm7 = 0;
+float valor_ppm8 = 0;
+float valor_ppm9 = 0;
+float valor_ppm10 = 0;
 //----------------------------------------------------------
 
 // Seta os pinos e a leitura serial
@@ -85,23 +85,24 @@ void loop() {
   if(start_command){                                                           // Entra no `if` caso o `start_command` for `True`
       reset_timer();                                                           // Reseta o timer     
       while ( ciclos >= count) {                                               // Inicia os ciclos de liberacao e recuperacao de gas
-        if(get_timer() > time_liberacao_ms && libera){
+        if(get_timer() > time_liberacao_ms*1000 && libera){
             liberacao();                                                       // Chama a funcao liberacao
             libera = false;                                                    // Fecha a valvula de liberacao do gas
             recupera = true;              
             reset_timer();                                                     // Reseta o timer
           }
-          if(get_timer() > time_recuperacao_ms && recupera){
+          if(get_timer() > time_recuperacao_ms*1000 && recupera){
             recuperacao();                                                     // Chama a funcao recuperacao
             recupera = false;                                                  // Abre a valvula para a liberacao de gas
             libera = true;
             count++;                                                           // Aumenta a contagem de ciclos em 1
             reset_timer();                                                     // Reseta o timer
           }
+        
         valor_sensor1 = analogRead(pinSensor1);                                  // Leitura analogica do sensor de gas
         valor_sensor_voltage1 = valor_sensor1 / 1024 * 5.0;                      // Conversao da leitura analogica para Volts
         valor_ppm1 = map(valor_sensor_voltage1 * 100, 0, 500, 200, 1000);        // Regra de tres para conversao da leitura em Volts para PPM
-        
+       
         valor_sensor2 = analogRead(pinSensor2);                                 
         valor_sensor_voltage2 = valor_sensor2 / 1024 * 5.0;                      
         valor_ppm2 = map(valor_sensor_voltage2 * 100, 0, 500, 200, 1000);        
@@ -136,9 +137,9 @@ void loop() {
 
         valor_sensor10 = analogRead(pinSensor10);
         valor_sensor_voltage10 = valor_sensor10 / 1024 * 5.0;                      
-        valor_ppm10 = map(valor_sensor_voltage10 * 100, 0, 500, 200, 1000);     
-        
-        sendToPython(&valor_ppm1,&valor_ppm2,&valor_ppm3,&valor_ppm4,&valor_ppm5,&valor_ppm6,&valor_ppm7,&valor_ppm8,&valor_ppm9,&valor_ppm10);                       // Envia o valor em PPM para a funcao `sendToPython`
+        valor_ppm10 = map(valor_sensor_voltage10 * 100, 0, 500, 200, 1000);    
+
+        sendToPython(&valor_ppm1,&valor_ppm2,&valor_ppm3,&valor_ppm4,&valor_ppm5,&valor_ppm6,&valor_ppm7,&valor_ppm8,&valor_ppm9,&valor_ppm10,numPlots);                       // Envia o valor em PPM para a funcao `sendToPython`
     
     }   
   }
@@ -149,30 +150,32 @@ void loop() {
 // Funcoes 
 //---------------------------------------------------------------------------------------------------------------
 // Recebe o valor em `data` e converte este valor em binario
-void sendToPython(double* data1, double* data2, double* data3,double* data4,double* data5, double* data6, double* data7,double* data8,double* data9, double* data10){ // Adicionar mais sensores  `double* data2`               
+void sendToPython(float* data1,float* data2,float* data3,float* data4,float* data5,float* data6,float* data7,float* data8,float* data9,float* data10, float numPlots){ // Adicionar mais sensores  `double* data2`               
   byte* byteData1 = (byte*)(data1);       // Sensor 1
-  byte* byteData2 = (byte*)(data2);       // Sensor 2
-  byte* byteData3 = (byte*)(data3);       // Sensor 3
-  byte* byteData4 = (byte*)(data4);       // Sensor 4
-  byte* byteData5 = (byte*)(data5);       // Sensor 5
-  byte* byteData6 = (byte*)(data6);       // Sensor 6
-  byte* byteData7 = (byte*)(data7);       // Sensor 7
-  byte* byteData8 = (byte*)(data8);       // Sensor 8
-  byte* byteData9 = (byte*)(data9);       // Sensor 9
-  byte* byteData10 = (byte*)(data10);    // Sensor 10
+  byte* byteData2 = (byte*)(data2);       // Sensor 1
+  byte* byteData3 = (byte*)(data3);       // Sensor 1
+  byte* byteData4 = (byte*)(data4);       // Sensor 1
+  byte* byteData5 = (byte*)(data5);       // Sensor 1
+  byte* byteData6 = (byte*)(data6);       // Sensor 1
+  byte* byteData7 = (byte*)(data7);       // Sensor 1
+  byte* byteData8 = (byte*)(data8);       // Sensor 1
+  byte* byteData9 = (byte*)(data9);       // Sensor 1
+  byte* byteData10 = (byte*)(data10);       // Sensor 1
+
+  numPlots = numPlots*4;
 
   byte buf[40] = {byteData1[0], byteData1[1], byteData1[2], byteData1[3],
-                 byteData2[0], byteData2[1], byteData2[2], byteData2[3],
-                 byteData3[0], byteData3[1], byteData3[2], byteData3[3],
-                 byteData4[0], byteData4[1], byteData4[2], byteData4[3],
-                 byteData5[0], byteData5[1], byteData5[2], byteData5[3],
-                 byteData6[0], byteData6[1], byteData6[2], byteData6[3],
-                 byteData7[0], byteData7[1], byteData7[2], byteData7[3],
-                 byteData8[0], byteData8[1], byteData8[2], byteData8[3],
-                 byteData9[0], byteData9[1], byteData9[2], byteData9[3],
-                 byteData10[0], byteData10[1], byteData10[2], byteData10[3]};
-
-  Serial.write(buf, 40);      // Buffer de x = 4(bytes) * Numero de sensores
+                  byteData2[0], byteData2[1], byteData2[2], byteData2[3],
+                  byteData3[0], byteData3[1], byteData3[2], byteData3[3],
+                  byteData4[0], byteData4[1], byteData4[2], byteData4[3],
+                  byteData5[0], byteData5[1], byteData5[2], byteData5[3],
+                  byteData6[0], byteData6[1], byteData6[2], byteData6[3],
+                  byteData7[0], byteData7[1], byteData7[2], byteData7[3],
+                  byteData8[0], byteData8[1], byteData8[2], byteData8[3],
+                  byteData9[0], byteData9[1], byteData9[2], byteData9[3],
+                  byteData10[0], byteData10[1], byteData10[2], byteData10[3]};
+//
+  Serial.write(buf, numPlots);      // Buffer de x = 4(bytes) * Numero de sensores
 }
 
 // Recebe os dados do Python
@@ -199,6 +202,11 @@ void parse_serial(){
 
     case 'c': {
       ciclos = value;
+      break;
+    }
+
+    case 'n': {
+      numPlots = value;
       break;
     }
 
---
+++ Recover.py
@@ -0,0 +1,476 @@
+from threading import Thread
+import serial  # importa biblioteca Serial
+import time  # importa biblioteca Time
+import collections
+import matplotlib.pyplot as plt  # importa biblioteca matplotlib
+import matplotlib.animation as animation
+import struct
+import copy
+import pandas as pd
+import csv
+from pathlib import Path
+from tkinter import *
+
+plt.style.use("seaborn-ticks")  # Seleciona o estilo do grafico
+# ['seaborn-ticks', 'ggplot', 'dark_background', 'bmh', 'seaborn-poster','seaborn-muted', '_classic_test',
+# 'seaborn-notebook', 'fast', 'seaborn', 'classic', 'Solarize_Light2', 'seaborn-dark', 'seaborn-pastel',
+# 'seaborn-paper', 'seaborn-colorblind', 'seaborn-bright', 'seaborn-talk', 'seaborn-dark-palette', 'tableau-colorblind10',
+# 'seaborn-whitegrid', 'fivethirtyeight', 'grayscale', 'seaborn-white', 'seaborn-deep', 'seaborn-darkgrid']
+
+
+class serialPlot:  # define classe serialPlot
+    def __init__(
+        self,
+        serialPort="COM12",
+        serialBaud=9600,
+        plotLength=100,
+        dataNumBytes=2,
+        numPlots=1,
+        tempo_exposicao=0,
+        tempo_recuperacao=0,
+        ciclos=0,
+        pltInterval = 0,
+        filename="filename",
+    ):
+        # variaveis definidas para a classe serialPlot
+        # ----------------------
+        self.port = serialPort
+        self.baud = serialBaud
+        self.plotMaxLength = plotLength
+        self.dataNumBytes = dataNumBytes
+        self.numPlots = numPlots
+        self.rawData = bytearray(numPlots * dataNumBytes)
+        self.dataType = None
+        if dataNumBytes == 2:
+            self.dataType = "h"  # 2 byte integer
+        elif dataNumBytes == 4:
+            self.dataType = "f"  # 4 byte float
+        self.data = []
+        for i in range(
+            numPlots
+        ):  # give an array for each type of data and store them in a list
+            self.data.append(collections.deque([0] * plotLength, maxlen=plotLength))
+        self.isRun = True
+        self.isReceiving = False
+        self.thread = None
+        self.plotTimer = 0
+        self.previousTimer = 0
+        self.tempo_exposicao = tempo_exposicao
+        self.tempo_recuperacao = tempo_recuperacao
+        self.ciclos = ciclos
+        self.pltInterval = pltInterval
+        self.tempo = 0
+        self.i = 0
+        self.filename = filename
+        self.txtData = []
+
+        # ----------------------
+
+        # Comeca a comunicacao serial com o arduino
+        print(
+            "Trying to connect to: "
+            + str(serialPort)
+            + " at "
+            + str(serialBaud)
+            + " BAUD."
+        )
+        try:
+            self.serialConnection = serial.Serial(serialPort, serialBaud, timeout=4)
+            print(
+                "Connected to " + str(serialPort) + " at " + str(serialBaud) + " BAUD."
+            )
+        except:
+            print(
+                "Failed to connect with "
+                + str(serialPort)
+                + " at "
+                + str(serialBaud)
+                + " BAUD."
+            )
+
+    def readSerialStart(self):
+        # Envia para o arduino as entradas de tempo_exposicao, tempo_recuperacao e ciclos
+        # ---------------------------------------------------------------------------
+        time.sleep(1)
+        self.serialConnection.write(f"i;l;{self.tempo_exposicao:05d};f\n".encode())
+        self.serialConnection.write(f"i;r;{self.tempo_recuperacao:05d};f\n".encode())
+        self.serialConnection.write(f"i;c;{self.ciclos:05d};f\n".encode())
+        self.serialConnection.write(f"i;n;{self.numPlots:05d};f\n".encode())
+        # ---------------------------------------------------------------------------
+
+        if self.thread == None:
+            self.thread = Thread(target=self.backgroundThread)
+            self.thread.start()
+            while self.isReceiving != True:  # Fica no loop ate comecar a receber dados
+                time.sleep(0.1)
+
+    def getSerialData(self, frame, lines, lineValueText, lineLabel, timeText):
+        # Plota no grafico o tempo e os dados recebidos dos sensores
+        # --------------------------------------------------------------
+        pltCount = 0
+        unity = 'sec'
+        if unity == 'sec':
+            pltUp = 1000
+        elif unity == 'milisec':
+            pltUp = 1
+        elif unity == 'microsec':
+            pltUp = 1
+        elif unity == 'hour':
+            pltUp = 1
+        elif unity == 'min':
+            pltUp = 1
+        elif unity == 'days':
+            pltUp = 1
+        while pltCount!= self.pltInterval:
+            self.serialConnection.write(b"i;s;00000;f\n")  # Inicia a leitura do sensor analogico no arduino
+            currentTimer = time.perf_counter()
+            self.plotTimer = int((currentTimer - self.previousTimer) * 1000)  # the first reading will be erroneous
+            if self.i == 0:
+                self.tempo = 0
+                self.i = 1
+            else:
+                self.tempo = round(self.tempo + self.plotTimer / 1000, 2)
+            self.txtData.append("  " + str(self.tempo) + "  ")
+            self.previousTimer = currentTimer
+            timeText.set_text("Plot Interval = " + str(self.plotTimer) + "ms")
+            privateData = copy.deepcopy(self.rawData[:])  # so that the 3 values in our plots will be synchronized to the same sample time
+            for i in range(self.numPlots):
+                data = privateData[(i * self.dataNumBytes) : (self.dataNumBytes + i * self.dataNumBytes)]
+                value, = struct.unpack(self.dataType, data)
+                self.data[i].append(value)  # we get the latest data point and append it to our array
+                lines[i].set_data(range(self.plotMaxLength), self.data[i])
+                lineValueText[i].set_text("[" + lineLabel[i] + "] = " + str(value))
+                self.txtData.append("   " + str(value) + "  ")
+            self.txtData.append("\n")
+            pltCount = pltCount +pltUp
+        # --------------------------------------------------------------
+
+    def backgroundThread(self):  # Recupera `data`
+        time.sleep(1.0)  # Da um tempo para o buffer armazenar os dados
+        self.serialConnection.reset_input_buffer()
+        while self.isRun:
+            self.serialConnection.readinto(self.rawData)
+            self.isReceiving = True
+
+    def close(self):
+        self.isRun = False
+        self.thread.join()
+        self.serialConnection.close()
+        print("Disconnected...")
+        file_out = open(str(self.filename) + ".txt", "a")
+        file_out.writelines(
+            "Tempo    Sensor1    Sensor2    Sensor3    Sensor4    Sensor5    Sensor6    Sensor7    Sensor8    Sensor9    Sensor10 \n"
+        )
+        file_out.writelines(self.txtData)
+        file_out.close()
+
+
+def main():
+    # Codigo do usuario
+    # ------------------
+    baudRate = 9600  # BaundRate do codigo em arduino
+    dataNumBytes = 4  # Number de bytes de 1 ponto de dado
+    # ------------------
+
+    # Entaradas do usuario sao armazenadas nas variaveis tempo_exposicao, tempo_recuperacao e ciclos
+    # --------------------------------------------------
+    class Application(Frame):
+        def __init__(self, master):
+            Frame.__init__(self, master)
+            self.grid()
+            self.create_widgets()
+
+        def create_widgets(self):
+            self.instruction = Label(
+                self,
+                text="Port name:",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=0, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Tempo exposição (s):",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=1, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Tempo recuperção (s):",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=2, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Numero de ciclos:",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=3, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Numero de amostragem por ciclos:",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=4, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Numero de sensores(max = 10):",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=5, column=0)
+
+            self.instruction = Label(
+                self,
+                text="Nome do arquivo txt:",
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.instruction.grid(row=6, column=0)
+
+            self.portName = Entry(self)
+            self.portName.grid(row=0, column=1, sticky=W)
+
+            self.Tempo_exposicao = Entry(self)
+            self.Tempo_exposicao.grid(row=1, column=1, sticky=W)
+
+            self.Tempo_recuperacao = Entry(self)
+            self.Tempo_recuperacao.grid(row=2, column=1, sticky=W)
+
+            self.ciclos = Entry(self)
+            self.ciclos.grid(row=3, column=1, sticky=W)
+
+            self.numero_amostragem = Entry(self)
+            self.numero_amostragem.grid(row=4, column=1, sticky=W)
+
+            self.numPlots = Entry(self)
+            self.numPlots.grid(row=5, column=1, sticky=W)
+
+            self.filename = Entry(self)
+            self.filename.grid(row=6, column=1, sticky=W)
+
+            self.simulation_button = Button(
+                self,
+                text="Tempo total",
+                command=self.simulation,
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.simulation_button.grid(row=7, column=2, sticky=W)
+
+            self.start_button = Button(
+                self,
+                text="Start",
+                command=self.start,
+                fg="black",
+                font=("arial", 10, "bold"),
+                padx=50,
+                pady=10,
+                bd=1,
+            )
+            self.start_button.grid(row=7, column=1, sticky=W)
+
+        def simulation(self):
+            tempo_exposicao = self.Tempo_liberacao.get()
+            tempo_recuperacao = self.Tempo_recuperacao.get()
+            ciclos = self.ciclos.get()
+            numero_amostragem = self.numero_amostragem.get()
+            portName = self.portName.get()
+            filename = self.filename.get()
+            numPlots = self.numPlots.get()
+
+            if (erro_handler(self,tempo_exposicao,tempo_recuperacao,ciclos,numero_amostragem,portName,numPlots,)== False):
+                tempo_exposicao = int(tempo_exposicao)
+                tempo_recuperacao = int(tempo_recuperacao)
+                ciclos = int(ciclos)
+                numero_amostragem = int(numero_amostragem)
+                tempo_total = (tempo_exposicao + tempo_recuperacao) * ciclos
+                self.instruction = Label(
+                    self,
+                    text="Tempo total do ensaio: " + str(tempo_total) + "s",
+                    fg="black",
+                    font=("arial", 10, "bold"),
+                    padx=50,
+                    pady=10,
+                    bd=1,
+                )
+                self.instruction.grid(row=6, column=3)
+
+        def start(self):
+            tempo_exposicao = self.Tempo_exposicao.get()
+            tempo_recuperacao = self.Tempo_recuperacao.get()
+            ciclos = self.ciclos.get()
+            numero_amostragem = self.numero_amostragem.get()
+            portName = self.portName.get()
+            filename = self.filename.get()
+            numPlots = self.numPlots.get()
+
+            if (erro_handler(self,tempo_exposicao,tempo_recuperacao,ciclos,numero_amostragem,portName,numPlots,)== False):
+                tempo_exposicao = int(tempo_exposicao)#segundos
+                tempo_recuperacao = int(tempo_recuperacao)#segundos
+                ciclos = int(ciclos)
+                numPlots = int(numPlots)
+                numero_amostragem = int(numero_amostragem)
+                maxPlotLength = int(((tempo_exposicao + tempo_recuperacao) * ciclos) + 1)  # Maximo valor do eixo x do grafico (tempo) em segundos
+                print(str(maxPlotLength))
+                def getUnity (maxPlotLength):
+                    if maxPlotLength < 60:
+                        unity = 'sec'
+                    elif maxPlotLength >= 60 and maxPlotLength < 3600 :
+                        unity == 'min'
+                    elif maxPlotLength >= 3600 and maxPlotLength < 86400:
+                        unity == 'hour'
+                    elif maxPlotLength >= 86400:
+                        unity == 'days'
+                    # elif maxPlotLength < 60:
+                    #     unity == 'min'
+                    # elif maxPlotLength < 60:
+                    #     unity == 'days'
+                pltInterval = int(((tempo_exposicao + tempo_recuperacao)*1000//numero_amostragem))  # Tempo em que e atualizado cada Plot do grafico
+                # --------------------------------------------------
+
+                s = serialPlot(
+                    portName,
+                    baudRate,
+                    maxPlotLength,
+                    dataNumBytes,
+                    numPlots,
+                    tempo_exposicao,
+                    tempo_recuperacao,
+                    ciclos,
+                    pltInterval,
+                    filename,
+                )  # Inizializa todas as varivaeis necessarias para a 'classe serialPLot'
+                s.readSerialStart()  # Comeca o backgroundThread
+
+                # Comeca a plotar no grafico
+                # ----------------------------------------------------------------------
+                xmin = 0  # Valor minimo do X do grafico
+                xmax = maxPlotLength  # Valor maximo do X do grafico
+                ymin = 200  # Valor minimo do y do grafico
+                ymax = 1000  # Valor maximo do y do grafico
+                fig = plt.figure()
+                ax = plt.axes(xlim=(xmin, xmax), ylim=(ymin, ymax))
+                ax.set_title("Projeto IC")  # Titulo do grafico
+                ax.set_xlabel("Tempo")  # Titulo do eixo x
+                ax.set_ylabel("Valor em PPM")  # Titulo do eixo y
+                nframes = int((tempo_exposicao + tempo_recuperacao)*ciclos*1000//pltInterval)
+
+                lineLabel = [
+                    "Sensor1",
+                    "Sensor2",
+                    "Sensor3",
+                    "Sensor4",
+                    "Sensor5",
+                    "Sensor6",
+                    "Sensor7",
+                    "Sensor8",
+                    "Sensor9",
+                    "Sensor10",
+                ]
+                style = ["r-","c-","b-","g-","y-","m-","k-","w-","p-","s-",]  # linestyles for the different plots
+                timeText = ax.text(0.50, 0.95, "", transform=ax.transAxes)
+                lines = []
+                lineValueText = []
+                for i in range(numPlots):
+                    lines.append(ax.plot([], [], style[i], label=lineLabel[i])[0])
+                    lineValueText.append(
+                        ax.text(0.70, 0.90 - i * 0.05, "", transform=ax.transAxes)
+                    )
+                anim = animation.FuncAnimation(
+                    fig,
+                    s.getSerialData,
+                    fargs=(lines, lineValueText, lineLabel, timeText),
+                    interval=pltInterval,
+                    frames = nframes,
+                    repeat = False
+                )  # Envia as variaveis para plotar o grafico
+
+                plt.legend(loc="upper left")  # Local da legenda no graafico
+                plt.show()
+                # ----------------------------------------------------------------------
+
+                s.close()  # Para de plotar o grafico
+
+    root = Tk()
+    root.title("IC")
+    root.geometry("900x400")
+    app = Application(root)
+
+    root.mainloop()
+
+
+def erro_handler(
+    self,
+    tempo_exposicao="",
+    tempo_recuperacao="",
+    ciclos="",
+    numero_amostragem="",
+    portName="",
+    numPlots="",
+):
+    if (tempo_exposicao == ""or tempo_recuperacao == ""or ciclos == ""or numero_amostragem == ""or portName == ""or numPlots == ""):
+        erro = Label(
+            self,
+            text="É preciso completar todos os espaços em branco",
+            fg="black",
+            font=("arial", 10, "bold"),
+        )
+        erro.grid(row=6, column=3)
+        return True
+    elif int(numPlots) > 10:
+        erro = Label(
+            self,
+            text="Passou o numero máximo de sensores",
+            fg="black",
+            font=("arial", 10, "bold"),
+        )
+        erro.grid(row=5, column=3)
+        return True
+    else:
+        erro = Label(
+            self,
+            text="Completar todos os espaços em branco",
+            fg="black",
+            font=("arial", 10, "bold"),
+        )
+        erro.grid(row=5, column=3)
+        erro.grid_remove()
+        return False
+
+
+if __name__ == "__main__":
+    main()
